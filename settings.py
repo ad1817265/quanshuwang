72d42c967ae0cc23a59ec6925b1633455d9a5409
ad1817265 <59492302+ad1817265@users.noreply.github.com>
2020-07-09 15:36:54 +0800

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,12 @@
+DATBASE_CONFIG={
+
+        "type":"mysql",
+        "config":{
+            "host":"127.0.0.1",
+            "port":3306,
+            "user":"root",
+            "password":"123",
+            "db":"novel",
+            "charset":"utf8"
+        }
+}
